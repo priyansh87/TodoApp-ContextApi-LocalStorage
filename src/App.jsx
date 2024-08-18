32e201424afb54564ed4e0e7b5dffaacf2265975
priyansh87 <priyansh56701@gmail.com>
2024-08-19 01:03:14 +0530

    done with context and local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState , useEffect} from "react"
 import { TodoProvider } from "./context";
+import { TodoForm, TodoItem } from "./components";
 
 function App() {
 
@@ -48,9 +49,17 @@ function App() {
             <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
             <div className="mb-4">
                 {/* Todo form goes here */} 
+                <TodoForm/>
             </div>
             <div className="flex flex-wrap gap-y-3">
                 {/*Loop and Add TodoItem here */}
+                {todos.map( (todo)=>{
+                  return (
+                    <div key={todo.id} className="w-full">
+                      <TodoItem todo={todo} />
+                    </div>
+                  )
+                } )}
             </div>
         </div>
       </div>
